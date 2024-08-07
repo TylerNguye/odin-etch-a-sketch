92dd94f0aee8b1b3d641eb1653aec63e0956d020
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-08-06 21:05:33 -0400

    Add borders

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ function createSquares() {
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
             const square = document.createElement("div")
+            square.classList.add("square")
+            square.classList.addEventListener('mouseenter', () => {
+                square.style.opacity = 1
+            })
+            // square.classList.addEventListener('mouseleave', )
             gridContainer.appendChild(square)
         }
     }
 }
+
+createSquares()
