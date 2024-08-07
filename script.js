85a6c202081e95ed1734df814cbb3ea26cc5efd0
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-08-06 23:14:47 -0400

    Add button to change number of squares
    Valid inputs are 1-100 inclusive

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const gridContainer = document.querySelector("#grid-container")
+const changeBtn = document.querySelector("#change-btn")
+changeBtn.addEventListener('click', () => {
+    const numSquares = prompt("Enter the number of squares. Min: 1 Max: 100", "16")
+    if (!Number.isInteger(Number(numSquares))) {
+        alert("Please enter an integer.")
+        return
+    }
+    else if (numSquares < 1) {
+        alert("Please enter a higher integer.")
+        return
+    }
+    else if (numSquares > 100) {
+        alert("Please enter a lower integer.")
+        return
+    }
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.firstChild)
+    }
+    createSquares(Number(numSquares))
+})
 
-function createSquares() {
-    const side = gridContainer.clientWidth / 16
-    for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++) {
+function createSquares(numSquares) {
+    const BORDERWIDTH = 1
+    const side = (gridContainer.clientWidth / numSquares) - BORDERWIDTH
+    for (let i = 0; i < numSquares; i++) {
+        for (let j = 0; j < numSquares; j++) {
             const square = document.createElement("div")
             initSquareAttributes(square, side)
             square.addEventListener('mouseenter', () => {
@@ -18,7 +39,7 @@ function createSquares() {
             square.addEventListener('mouseleave', () => {
                 square.classList.add("paintable")
             })
-            const squareBorder = initSquareBorder(side)
+            const squareBorder = initSquareBorder(side + BORDERWIDTH)
             squareBorder.appendChild(square)
             gridContainer.appendChild(squareBorder)
         }
@@ -36,9 +57,9 @@ function initSquareAttributes(square, side) {
 function initSquareBorder(side) {
     const squareBorder = document.createElement("div")
     squareBorder.classList.add("square-border")
-    squareBorder.style.width = side + 1 + "px"
+    squareBorder.style.width = side + "px"
     squareBorder.style.height = side + 1 + "px"
     return squareBorder
 }
 
-createSquares()
+createSquares(16)
